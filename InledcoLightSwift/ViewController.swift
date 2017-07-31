c63386d3673204addb4df494cc7969ec8f885e12
huangzhengguo <huangzhengguo@huangdemac-mini.local>
2017-07-31 08:25:56 +0800

    Initial Commit

---
+++ InledcoLightSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InledcoLightSwift
+//
+//  Created by huang zhengguo on 2017/7/31.
+//  Copyright © 2017年 huang zhengguo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
